9dcb78aa18070325c7e9490586ff7ea6d0df8599
Victor Oginskis <viktorsoginskis@gmail.com>
2018-09-25 20:20:22 +0300

    upd

---
+++ v2/lab-02/k8s-wordsmith-demo/db/words.sql
@@ -0,0 +1,26 @@
+CREATE TABLE nouns (word TEXT NOT NULL);
+CREATE TABLE verbs (word TEXT NOT NULL);
+CREATE TABLE adjectives (word TEXT NOT NULL);
+
+INSERT INTO nouns(word) VALUES
+  ('dators'),
+  ('koks'),
+  ('zīmulis'),
+  ('ūdens'),
+  ('cilvēks'),
+  ('pele');
+
+INSERT INTO verbs(word) VALUES
+  ('lieto'),
+  ('skaitļo'),
+  ('aug'),
+  ('raksta'),
+  ('ir'),
+  ('ēd');
+
+INSERT INTO adjectives(word) VALUES
+  ('akls'),
+  ('dzidrs'),
+  ('centīgs'),
+  ('ciets'),
+  ('vecs');
